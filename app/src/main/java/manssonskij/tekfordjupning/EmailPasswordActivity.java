b35f7c9d8d0d7ae6a2fbc89abda54984060cb515
manssonskij <anton.mansson@gmail.com>
2016-12-07 15:22:24 +0100

    firebase auth

---
+++ app/src/main/java/manssonskij/tekfordjupning/EmailPasswordActivity.java
@@ -0,0 +1,211 @@
+package manssonskij.tekfordjupning;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+/**
+ * Created by Anton on 2016-12-06.
+ */
+
+public class EmailPasswordActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private static final String TAG = "RegisterActivity";
+
+    private AutoCompleteTextView mEmailView;
+    private AutoCompleteTextView mNameView;
+    //private TextInputEditText mNameView;
+    //private TextInputEditText mPasswordView;
+    private EditText mPasswordView;
+    private View mProgressView;
+    private View mLoginFormView;
+    // private FirebaseAuth mAuth;
+    // private FirebaseAuth.AuthStateListener mAuthListener;
+    private Boolean mAllowNavigation = true;
+    private ProgressBar progressBar;
+    /*
+     * Resources and examples for how to implement Firebase Auth
+     *
+     * https://firebase.google.com/docs/auth/android/password-auth
+     *
+     *
+     *
+     * Short and efficient
+     * http://www.androidhive.info/2016/06/android-getting-started-firebase-simple-login-registration-auth/
+     * https://github.com/firebase/quickstart-android/blob/master/auth/app/src/main/java/com/google/firebase/quickstart/auth/EmailPasswordActivity.java
+     *
+     */
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_emailpassword);
+
+
+        // Views
+        // changed to AutoCompleteTextView from textinputedittext
+        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
+        //mNameView = (TextInputEditText) findViewById(R.id.name);
+        mNameView = (AutoCompleteTextView) findViewById(R.id.name);
+        mPasswordView = (EditText) findViewById(R.id.password);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        // Buttons
+
+        findViewById(R.id.email_sign_in_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = mEmailView.getText().toString().trim();
+                String password = mPasswordView.getText().toString().trim();
+                /**
+                 * Check that values exist, code from:
+                 * http://www.androidhive.info/2016/06/android-getting-started-firebase-simple-login-registration-auth/
+                 */
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "You need to enter an email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "You need to enter a password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (password.length() < 6) {
+                    Toast.makeText(getApplicationContext(), "Minimum 6 characters!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                createAccount();
+            }
+        });
+        findViewById(R.id.email_register_button).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                createAccount();
+            }
+        });
+
+        //findViewById(R.id.email_create_account_button).setOnClickListener(this);
+        //findViewById(R.id.sign_out_button).setOnClickListener(this);
+
+        // Should be in onCreate
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    private void createAccount() {
+
+
+        // Get values from UI
+        String email = mEmailView.getText().toString().trim();
+        String password = mPasswordView.getText().toString().trim();
+
+
+
+
+
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+                        progressBar.setVisibility(View.GONE);
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(EmailPasswordActivity.this, R.string.auth_failed,
+                                    Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            startActivity(new Intent(EmailPasswordActivity.this, ItemListScrollingActivity.class));
+                        }
+
+                        // ...
+                    }
+                });
+
+    }
+
+    private void signIn() {
+
+        String email = mEmailView.getText().toString().trim();
+        String password = mNameView.getText().toString().trim();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithEmail:failed", task.getException());
+                            Toast.makeText(EmailPasswordActivity.this, R.string.auth_failed,
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+}
--- app/src/main/java/manssonskij/tekfordjupning/MainActivity.java
+++ app/src/main/java/manssonskij/tekfordjupning/MainActivity.java
@@ -1,5 +1,6 @@
 package manssonskij.tekfordjupning;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,12 +9,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -24,6 +28,13 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view){
+                startActivity(new Intent(MainActivity.this, EmailPasswordActivity.class));
             }
         });
     }
